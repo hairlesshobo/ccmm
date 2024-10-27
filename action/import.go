0bdbde0921e3121b9f073fecb333b0e20c85ea47
Steve Cross <flip@foxhollow.cc>
2024-10-27 18:58:53 -0400

    check volume path existence

--- action/import.go
+++ action/import.go
@@ -3,6 +3,7 @@ package action
 import (
 	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/hairlesshobo/go-import-media/model"
 	"github.com/hairlesshobo/go-import-media/processor"
@@ -10,6 +11,11 @@ import (
 
 func Import(params model.ImportVolume) {
 	slog.Info(fmt.Sprintf("Beginning import for volume '%s'", params.VolumePath))
+
+	if stat, err := os.Stat(params.VolumePath); err != nil || !stat.IsDir() {
+		slog.Error(fmt.Sprintf("Directory not found: %s", params.VolumePath))
+		os.Exit(1)
+	}
 
 	processors := processor.FindProcessors(params.VolumePath)
 	processor.ProcessSources(processors, params.DryRun)
