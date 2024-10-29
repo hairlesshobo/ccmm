6d7d5288191bdd2458eef3e24d58293dc7472f40
Steve Cross <flip@foxhollow.cc>
2024-10-29 00:20:29 -0400

    add notes

--- util/deviceControl_linux.go
+++ util/deviceControl_linux.go
@@ -30,9 +30,20 @@ func MountVolume(device string, mountPath string) bool {
 }
 
 func UnmountVolume(device string) bool {
+	// this should be provided with the full path of the mounted partition,
+	// ex: /dev/sda1
+
+	//  udisksctl unmount --block-device %s0
 	return false
 }
 
 func PowerOffDevice(device string) bool {
+	// this needs to be provided the path to the block device itself,
+	// for example /dev/sda (without the partition index). will need to
+	// look this value up. one way is this:
+	//  readlink -f "/sys/class/block/sda1/.."
+	// note 2: power off might work on the partitoin device, double check it
+
+	//  udisksctl power-off --block-device %s0
 	return false
 }
